0cd8e7b873ccb8b0f2561adac872352fa5253642
Mikhail Kirichkov <mihailkirichkov@yandex.ru>
2021-03-15 16:06:44 +0300

    fix: фиксы первой итерации

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 let popup = document.querySelector('.popup');
 let openPopupButton = document.querySelector('.profile__edit-button');
-let closePopupButton = document.querySelector('.popup__close-button');
+let closePopupButton = popup.querySelector('.popup__close-button');
 let popupOverlay = document.querySelector('.popup__overlay');
 let defaultName = document.querySelector('.profile__name');
 let defaultJob = document.querySelector('.profile__function');
@@ -9,18 +9,16 @@ let nameInput = formElement.querySelector('.popup__name');
 let jobInput = formElement.querySelector('.popup__function');
 let saveButton = document.querySelector('.popup__close-button');
 
-nameInput.value = defaultName.textContent;
 
-jobInput.value = defaultJob.textContent;
 
 function openPopup() {
-  popup.classList.add('popup_active');
+  popup.classList.add('popup_opened');
+  nameInput.value = defaultName.textContent;
+  jobInput.value = defaultJob.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove('popup_active');
-  nameInput.value = defaultName.textContent;
-  jobInput.value = defaultJob.textContent;
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
@@ -37,5 +35,3 @@ closePopupButton.addEventListener('click', closePopup);
 popupOverlay.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
-
-saveButton.addEventListener('click', formSubmitHandler);
