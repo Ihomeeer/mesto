f83ff3be46121fd4515b8899d83ef7e38c175e89
Mikhail Kirichkov <mihailkirichkov@yandex.ru>
2021-05-07 14:59:30 +0300

    начинаю тестировать PopupWithImage

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,5 +1,23 @@
 export default class PopupWithForm extends Popup {
-  constructor () {
-    
+  constructor (popupSelector, submitHandler) {
+    super(popupSelector);
+    this._submitHandler = submitHandler;
+    this._form = this.popup.querySelector('.popup__main-form');
+  }
+
+
+  _getInputValues = () => {
+    const values = {};
+    const inputs = Array.from(this._form.querySelector('.popup__input'));
+    inputs.forEach(input => {
+      console.log(input.name)
+      values.input.name = input.value;
+    })
+    console.log(inputs)
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener('submit', this._submitHandler);
   }
 }
